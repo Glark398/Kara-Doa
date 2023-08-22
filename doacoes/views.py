c154fbfaaaec7e7e11034c23e9e3e4247fcde6b5
Rafael Ribeiro <klkp.398@gmail.com>
2023-08-22 11:20:39 +0200

    a

--- doacoes/views.py
+++ doacoes/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'app/index.html')
+    return render(request, 'doacoes/index.html')
 
 def equipe(request):
-    return render(request, 'app/equipe.html')
+    return render(request, 'doacoes/equipe.html')
 
 def sobre(request):
-    return render(request, 'app/sobre.html')
+    return render(request, 'doacoes/sobre.html')
 
 
